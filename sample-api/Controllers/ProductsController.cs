ef64317ab56dfe1bcb9cf364f2362e8ac650a289
Swapnil Gaikwad <swpnlgkwd@hotmail.com>
2021-07-12 11:14:32 +0530

    Day15 Web API Project Creation & Initial Concepts added

---
+++ sample-api/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using sample_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sample_api.Controllers
+{
+    // http://localhost:5001/api/Products
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET :  api/products
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return new List<Product>
+            {
+                 new Product{ ProductID=1,Name="Chess"},
+                 new Product{ ProductID=2,Name="Cricket Bat"}
+            };
+        }
+
+        // GET By Id:api/products/{id}
+        [HttpGet("{id}")]
+        public Product GetProductById(int id)
+        {
+            return new Product
+            {
+                Name = "Product " + id
+            };
+        }
+    }
+}
---
+++ sample-api/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sample_api.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string  Name { get; set; }
+        public string  Description { get; set; }
+        public int  Price { get; set; }
+        public string  Category { get; set; }
+        public DateTime MfgDate { get; set; }
+    }
+}
